9f11b7cf73f3579d3ed7a0e4db7c4dca158e62ac
Droffilc13 <62885511+Droffilc13@users.noreply.github.com>
2023-10-14 21:32:33 +0800

    Create backend apis to access toy chat data

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 `git init` is to initialise the nodejs server. package.json will be initialise under webproj folder. This is where we can initialise the entry point of our backend server
 
+`npm init` initialises a package.json file
+
 `npm i <package name>`: using npm to install a certain package
 
 `node backend/server.js` to start the server. However, we can create a custom script in package.json. map "name" to the command => call npm <name>
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 import express from "express";
-import { chats } from "./data/data.js";
+import chats from "./data/data.js";
+import dotenv from 'dotenv';
+import path from 'path';
 
 const app = express()
+
+const __dirname = path.resolve()
+const envPath = {path : path.join(__dirname, '/.env')}
+dotenv.config(envPath)
 
 // Creation of API
 app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-// app.get("/master/chats", (req,res) => {
-//     res.send(chats)
-// })
+app.get("/chats", (req, res) => {
+    res.send(chats)
+})
 
-app.listen(5000, console.log("Server Started on PORT 5000"))
+app.get("/chats/:identifier", (req, res) => {
+    chat = chats.find(c => c._id == req.params.identifier)
+    res.send(chat)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`))
 
