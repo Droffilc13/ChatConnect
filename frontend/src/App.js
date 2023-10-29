bef651d853ba1242121b30ece1953649c6db2d0b
Droffilc13 <62885511+Droffilc13@users.noreply.github.com>
2023-10-30 01:15:43 +0800

    Build the tab functionality for login and sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import axios from "axios";
+import './App.css';
 
 import HomePage from './pages/HomePage'
 import ChatPage from "./pages/ChatPage";
+
 
 function App() {
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Container, Box, Text } from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 
 const HomePage = () => {
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container maxWidth='xl' centreContent>
+      <Box        
+        d='flex'
+        justifyContent="center"
+        padding={3}
+        fontFamily="Arial"
+        background={"white"}
+        m = "40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize='3xl' fontFamily='work sans' color='black' align="center">ChatConnect</Text>
+      </Box>
+      <Box 
+        background="white"
+        padding={4}
+        w="100%"
+        borderRadius="1g"
+        borderWidth="1px"
+      >
+        <Tabs variant='soft-rounded'>
+          <TabList marginBottom='1em'>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>{1}</TabPanel>
+            <TabPanel>{2}</TabPanel>
+          </TabPanels>
+        </Tabs>
+
+      </Box>
+    </Container>
   )
 }
 
