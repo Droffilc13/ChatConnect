da83bdee84976375bbb7fffe79998a787df278b5
Droffilc13 <62885511+Droffilc13@users.noreply.github.com>
2023-10-30 01:14:27 +0800

    Tidy up format of model files

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -2,8 +2,14 @@ import mongoose from 'mongoose';
 
 const chatModel = mongoose.Schema(
     {
-        chatName: { type: String, trim: true },
-        isGroupChat: { type: Boolean, default: false },
+        chatName: { 
+            type: String, 
+            trim: true 
+        },
+        isGroupChat: { 
+            type: Boolean, 
+            default: false 
+        },
         users: [{
             type: mongoose.Schema.Types.ObjectId,
             ref: "Users"
--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -3,9 +3,18 @@ import mongoose from 'mongoose';
 
 const messageModel = mongoose.Schema(
     {
-        sender : {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-        content: {type: String, trim: true},
-        chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat"}
+        sender : {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User"
+        },
+        content: {
+            type: String, 
+            trim: true
+        },
+        chat: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "Chat"
+        }
     }, 
     {
         timestamps: true
