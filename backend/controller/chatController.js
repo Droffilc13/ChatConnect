31e64d381143eb1e8245d0d89d11b6efca4e7550
Droffilc13 <62885511+Droffilc13@users.noreply.github.com>
2024-01-14 17:33:18 +0800

    Create endpoint of fetching chats and creating group

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -54,11 +54,51 @@ const accessChat = asyncHandler( async (req, res) => {
 })
 
 const fetchChats = asyncHandler( async (req, res) => {
-    console.log(fetchChats);    
+    try {
+        // TODO: probably need to sort the chats chronologically
+        // TODO2: what is the use of User.populate(....)
+        const relevantChats = await Chat.find({users: {$elemMatch: {$eq: req.user._id}}})
+            .populate("users", "-password")
+            .populate("latestMessage")
+            .populate("groupAdmin", "-password")
+        res.send(relevantChats);
+    } catch (e) {
+        res.status(400);
+        throw new Error(e.message);
+    }
 })
 
 const createGroupChat = asyncHandler( async (req, res) => {
-    console.log("createGroupChat")
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please Fill out all the fields"});
+    }
+
+    const users = JSON.parse(req.body.users);
+
+    if(users.length < 2) {
+        return res.status(400).send("More than 2 users are require dto form a group chat");
+    }
+
+    users.push(req.user);
+
+    try {
+        const groupChat = await Chat.create({
+            chatName: req.body.name,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req.user,
+        })
+
+        const fullGroupChat = await Chat.findOne({
+            _id: groupChat._id
+        }).populate("users", "-password")
+        .populate("latestMessage")
+        .populate("users", "groupAdmin");
+
+        res.status(200).send(fullGroupChat);
+    } catch(e) {
+        throw new Error(e.message);
+    }
 })
 
 const renameGroup = asyncHandler( async (req, res) => {
